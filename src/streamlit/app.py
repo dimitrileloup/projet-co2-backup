4768a439b2e98e16663495014d392b116f1b297c
dimitrileloup <37692252+dimitrileloup@users.noreply.github.com>
2025-07-17 15:18:48 +0200

    MAJ streamlit

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -799,6 +799,8 @@ try:
 
         try:
             model = joblib.load("models/" + selected_model_file)
+            if environnement == "cloud":
+                model = joblib.load("src/streamlit/models/" + selected_model_file)
             # X_test = df_nettoye.drop(columns=['CO2', 'Marque', 'Modèle', 'Consommation carburant'], axis=1)
             # y_test = df_nettoye['CO2']
             # X_test = pd.read_csv('models/X_test.csv')
@@ -830,6 +832,8 @@ try:
             results = []
             for model_file in model_files:
                 model = joblib.load("models/" + model_file)
+                if environnement == "cloud":
+                    model = joblib.load("src/streamlit/models/" + selected_model_file)
 
                 y_pred = model.predict(X_test)
 
