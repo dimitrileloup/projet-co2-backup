619c903400f986eccd1cc520b29754177da7d670
dimitrileloup <37692252+dimitrileloup@users.noreply.github.com>
2025-07-18 09:36:06 +0200

    correction

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -55,7 +55,7 @@ else:
     X_test = pd.read_csv('https://huggingface.co/datasets/dleloup/x_test/resolve/main/X_test.csv')
     y_test = pd.read_csv('https://huggingface.co/datasets/dleloup/x_test/resolve/main/y_test.csv').squeeze()   
     X_train = pd.read_csv('https://huggingface.co/datasets/dleloup/x_train/resolve/main/X_train.csv')
-    y_train = pd.read_csv('https://huggingface.co/datasets/dleloup/x_train/resolve/main/X_train.csv').squeeze()   
+    y_train = pd.read_csv('https://huggingface.co/datasets/dleloup/x_train/resolve/main/y_train.csv').squeeze()   
 
 # csv_path_dataset_nettoye = "datasets/datas_nettoyees_model_FR.csv"
 # # Nous passons par Kaggle car le dataset ne peut être envoyé sur Github : il est trop volumineux. Cela est plus rapide en local aussi.
